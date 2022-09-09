a3b1d2169b497bbceadffad26edd23e189a9a002
Thinker <alphacentuarian@gmail.com>
2022-09-09 16:32:31 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+
+{
+	char l;
+
+
+	l = 'a';
+
+	while (l <= 'z')
+
+	{
+		if (l == 'q' || l == 'e')
+
+			continue;
+		else
+		{
+			putchar(l);
+
+			l++;
+		}
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
