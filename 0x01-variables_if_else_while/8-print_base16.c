5f734a55aa32f1e8ca5c223828f7cfb82308e8ce
Thinker <alphacentuarian@gmail.com>
2022-09-09 18:54:51 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char l;
+
+	char j;
+
+	j = '1';
+
+	l = 'a';
+
+	while (j <= '9')
+	{
+	
+		putchar(j);
+
+		j++;
+
+	}
+
+	while (l < 'g')
+	{
+
+		putchar(l);
+
+		l++;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
