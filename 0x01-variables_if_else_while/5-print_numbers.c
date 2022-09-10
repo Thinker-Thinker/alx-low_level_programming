d239c8074a905c0d64c60f081d8a8228d7b587d5
Thinker <alphacentuarian@gmail.com>
2022-09-09 17:01:39 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+
+{
+	int l;
+
+
+	l = 0;
+
+	while (l <= 9)
+
+	{
+
+		printf("%i", l);
+
+		l++;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
