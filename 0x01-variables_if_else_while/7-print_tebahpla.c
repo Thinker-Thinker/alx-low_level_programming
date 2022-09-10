156785f85ceeb08532131783fdf650eb38e201d3
Thinker <alphacentuarian@gmail.com>
2022-09-09 18:43:06 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+
+{
+	char l;
+
+	char j;
+
+	l = 'z';
+
+	while (l >= 'a')
+
+	{
+
+		putchar(l);
+
+		l--;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
