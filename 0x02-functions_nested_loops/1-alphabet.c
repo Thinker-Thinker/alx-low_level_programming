bacb696b8fb6ff8b4f84a6d6fb68fa8b1e4ebae2
Thinker <alphacentuarian@gmail.com>
2022-09-13 15:25:58 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -15,3 +15,19 @@ void print_alphabet(void)
 	_putchar('\n');
 	return;
 }
+
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+
+int main(void)
+
+{
+
+	print_alphabet();
+
+	return (0);
+
+}
