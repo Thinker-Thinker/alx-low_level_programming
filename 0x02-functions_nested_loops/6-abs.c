3107de34f33a78483f37f7e94e0c98e5ca0463b1
Thinker <alphacentuarian@gmail.com>
2022-09-13 16:58:15 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _abs - Entry point
+*@int: test integer
+* Return: Always 0 (Success)
+*/
+int _abs(int)
+{
+	int n;
+
+	if (n < 0)
+	{
+		n = n * -1;
+		return (n);
+	}
+return (n);
+}
+
