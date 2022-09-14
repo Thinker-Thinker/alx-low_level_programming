9ebeb190ef6f9166ffaa8e35937c4992dd276726
Thinker <alphacentuarian@gmail.com>
2022-09-13 19:10:59 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* jack_bauer - Entry point
+*
+* Return: Always 0 (Success)
+*/
+void times_table(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9' ; j++)
+		{
+			k = n * j;
+			_putchar(k);			
+		}
+
+	}
+}
+
