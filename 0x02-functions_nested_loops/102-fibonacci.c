193f4b4b0b0f837dbb5cbfcaa99610e235d59a31
Thinker <alphacentuarian@gmail.com>
2022-09-14 09:28:59 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+	int fib;
+	int temp;
+	int count;
+	
+	i = 0;
+	fib = 1;
+	temp= 0;
+	count = 0;
+
+	while(count <= 50)
+	{
+		temp = fib;
+		fib = fib + i;
+		printf("%i, ", fib);
+		i = temp;
+		count++;
+	}
+	printf("%i\n", fib + i);
+	return (0);
+}
+
