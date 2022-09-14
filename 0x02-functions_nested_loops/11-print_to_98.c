7bea4f6633183f945115f24d52e868fda32ca8fa
Thinker <alphacentuarian@gmail.com>
2022-09-14 01:02:16 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,15 +7,15 @@
 */
 void print_to_98(int n)
 {
-	 	while (n != 98)
-		{
-			printf("%i, ", n);
-			if (n < 98)
-				n++;
-			else
-				n--;
-		}
+while (n != 98)
+{
+	printf("%i, ", n);
+	if (n < 98)
+		n++;
+	else
+		n--;
+}
 
-		printf("98\n");
+printf("98\n");
 }
 
