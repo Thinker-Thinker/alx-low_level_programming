3f375a09c8bf5a630bf772af2b0a604928d30c62
Thinker <alphacentuarian@gmail.com>
2022-09-13 17:38:48 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+* jack_bauer - Entry point
+*
+* Return: Always 0 (Success)
+*/
+void jack_bauer(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = '0'; i <= '2'; i++)
+	{
+		for (j = '0'; i <= '9' ; i++)
+		{
+			for (k = '0'; k < '6'; k++)
+			{
+				for (l = '0'; l <= '9'; l++)
+					if (i == '2' && j == '3' && k == '5' && j == '9')
+						break;
+				else
+				{
+					_putchar(i);
+					_putchar(j);
+					_putchar(':');
+					_putchar(k);
+					_putchar(l);
+					_putchar('\n');
+				}
+			}
+		
+		}
+		
+	}
+}
+
