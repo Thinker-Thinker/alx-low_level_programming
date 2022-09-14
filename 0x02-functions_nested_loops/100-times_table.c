380f64d6823702502549d7a99005a3914eb5addb
Thinker <alphacentuarian@gmail.com>
2022-09-14 05:21:21 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,42 +15,39 @@ void print_times_table(int n)
 		return;
 	else
 	{
-	for (i = 0; i <= n; i++)
-	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			k = i * j;
-			if (j == 0)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar('0');
+				k = i * j;
+				if (j == 0)
+					_putchar('0');
+				else if (k >= 10 && k <= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k % 10 + '0');
+					_putchar(k / 10 + '0');
+				}
+				else if (k >=100)
+					_putchar(',');
+					_putchar(' ');
+					_putchar(k % 100 + '0');
+					_putchar(k % 10 + '0');
+					_putchar(k / 10 + '0');
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				}
 			}
-			else if (k >= 10 && k <= 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k % 10 + '0');
-				_putchar(k / 10 + '0');
-			}
-			else if (k >=100)
-				_putchar(',');
-				_putchar(' ');
-				_putchar(k % 100 + '0');
-				_putchar(k % 10 + '0');
-				_putchar(k / 10 + '0');
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
-	}
 	
-
 	}
 }
 
