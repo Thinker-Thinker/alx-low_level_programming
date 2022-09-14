4da813ef7f78d205ba5b2229343ccd5a3033c561
Thinker <alphacentuarian@gmail.com>
2022-09-13 18:14:52 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -19,19 +19,15 @@ void jack_bauer(void)
 			{
 				for (l = '0'; l <= '9'; l++)
 				{
-					if (i == '2' && j == '4' && k == '0' && l == '0')
-					{	i = '3';
+					_putchar(i);
+					_putchar(j);
+					_putchar(':');
+					_putchar(k);
+					_putchar(l);
+					_putchar('\n');
+
+					if (i == '2' && j == '3' && k == '5' && l == '9')
 						break;
-					}
-					else
-					{
-						_putchar(i);
-						_putchar(j);
-						_putchar(':');
-						_putchar(k);
-						_putchar(l);
-						_putchar('\n');
-					}
 				}
 			}
 		}
