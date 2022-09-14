37960a969ec9c5860f05297a1d845811e8bbc787
Thinker <alphacentuarian@gmail.com>
2022-09-14 08:15:59 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,5 +1,27 @@
-#include <stdio.h>
 #include "main.h"
+/**
+* print_number- Entry point
+*@n: integer
+* Return: Always 0 (Success)
+*/
+
+void print_number(int n)
+{
+	if (n < 10)
+		_putchar(n +'0');
+	if (n >= 10 && n < 100);
+	{
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	if (n >= 100 && n <= 1000)
+	{
+		_putchar(n / 100 + '0');
+		_putchar((n % 100) / 10 + '0');
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+}
 /**
 * print_times_table - Entry point
 *@n: integer
@@ -7,11 +29,9 @@
 */
 void print_times_table(int n)
 {
-	int i; 
-	int j; 
-	int k;
+	int i; int j; int k;
 
-	if (n >15 )
+	if (n >15
 	{
 		for (i = 0; i <= n; i++)
 		{
@@ -19,19 +39,31 @@ void print_times_table(int n)
 			{
 				k = i * j;
 				if (j == 0)
-				printf("0");
-
+					_putchar('0');
 				else if (k >= 10 && k < 100)
-					printf(",  %i", k);
-
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					print_number(k);
+				}
 				else if (k >= 100)
-					printf(", %i", k);
+				{
+					_putchar(',');
+					_putchar(' ');
+					print_number(k);
+				}
 				else
-					printf(",   %i", k);
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				}
 			}
 			_putchar('\n');
 		}
 	
 	}
 }
-
