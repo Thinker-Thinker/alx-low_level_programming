d317ce2744d53dea07332841734394ac660f82df
Thinker <alphacentuarian@gmail.com>
2022-09-13 19:22:12 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,14 +10,16 @@ void times_table(void)
 	int j;
 	int k;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = '0'; j <= '9' ; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			k = i * j;
-			_putchar(k);
-			if (i != '9' && j != '9')
+			_putchar(k / 10 + '0');
+			_putchar(k % 10 + '0');
+			if (i != 9 && j != 9)
 			{
+				_putchar(',')
 				_putchar(' ');
 				_putchar(' ');
 			}
