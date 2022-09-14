267fb70913418612fb15e791547010bff608fb75
Thinker <alphacentuarian@gmail.com>
2022-09-14 11:10:19 -0700

    q

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -16,16 +16,20 @@ int main(void)
 	fib = 1;
 	temp = 0;
 	count = 1;
-
 	while (count < 98)
 	{
 		temp = fib;
 		fib = fib + i;
-		printf("%li, ", fib);
+		if (fib >= 100000000000000)
+		{
+			printf("%li%li", fib / 100000000000000, fib %100000000000000)
+		}
+		else
+			printf("%li, ", fib);
 		i = temp;
 		count++;
 	}
-	printf("%li\n", fib + i);
+	printf("%li%li\n", fib / 100000000000000, fib % 100000000000000);
 	return (0);
 }
 
