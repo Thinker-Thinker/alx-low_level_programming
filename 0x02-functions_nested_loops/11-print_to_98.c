9e38d7c6fd940dbc0cec38d33ddab442808a8931
Thinker <alphacentuarian@gmail.com>
2022-09-13 20:33:55 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_to_98 - Entry point
+*@n: integer
+* Return: Always 0 (Success)
+*/
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+	 	while (n <= 98)
+		{
+			printf(n);
+			n++;
+		}
+	}
+	else 
+	{
+		while (n >= 98)
+		{
+			printf(n);
+			n--;
+		}
+	}
+}
+
