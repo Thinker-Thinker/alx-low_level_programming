ec3335a3f9a19ee2e610b54224fdde399ee8ee70
Thinker <alphacentuarian@gmail.com>
2022-09-15 15:16:19 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _isupper - Entry point
+*@c: character value of integer c
+* Return: Always 0 (Success)
+*/
+int _isdigiti(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+return (0);
+}
+
