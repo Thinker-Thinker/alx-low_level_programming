40718c7b31ee00cac4b80465459dee3afcd2ae5e
Thinker <alphacentuarian@gmail.com>
2022-09-15 15:54:17 -0700

    main.h

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,12 +16,10 @@ void more_numbers(void)
 			if (i >= 10)
 			{
 				_putchar(i / 10 + '0');
-				_putchar(i % 10 + '0');
 			}
-			_putchar(i + '0');
+			_putchar(i % 10 + '0');
 		}
 		_putchar('\n');
 	}
-_putchar('\n');
 }
 
