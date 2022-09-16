d6cf22f2dab7d710b67e337e8d8d45ac1e7fa0c5
Thinker <alphacentuarian@gmail.com>
2022-09-15 20:22:54 -0700

    main.h

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,6 +5,7 @@
 *@n:integer
 * Return: Always 0 (Success)
 */
+
 void print_number(int n)
 {
 	if (n < 0)
@@ -12,16 +13,6 @@ void print_number(int n)
 		_putchar('-');
 		n = n * -1;
 	}
-	if (n >= 1000000000)
-		_putchar((n % 10000000000) / 1000000000 + '0');
-	if (n >= 100000000)
-		_putchar((n % 1000000000) / 100000000 + '0');
-	if (n >= 10000000)
-		_putchar((n % 100000000) / 10000000 + '0');
-	if (n >= 1000000)
-		_putchar((n % 10000000) / 1000000 + '0');
-	if (n >= 100000)
-		_putchar((n % 1000000) / 100000 + '0');
 	if (n >= 10000)
 		_putchar((n % 100000) / 10000 + '0');
 	if (n >= 1000)
