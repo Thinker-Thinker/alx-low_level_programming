a2ed1d233f4c8df9cc37ea274fbaa5e71f0b24e6
Thinker <alphacentuarian@gmail.com>
2022-09-15 17:13:14 -0700

    main.h

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* print_square - Entry point
+*@size: integer
+* Return: Always 0 (Success)
+*/
+void print_square(int size);
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,6 +4,8 @@
 
 int mul(int a, int b);
 
+void print_square(int size);
+
 void print_diagonal(int n);
 
 void print_line(int n);
