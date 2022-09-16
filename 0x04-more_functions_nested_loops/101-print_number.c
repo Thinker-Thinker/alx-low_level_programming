233b3466b0463e3e62a788f266d0b1083b6740c9
Thinker <alphacentuarian@gmail.com>
2022-09-15 20:09:10 -0700

    main.h

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -28,7 +28,7 @@ void print_number(int n)
 		_putchar((n % 10000) / 1000 + '0');
 	if (n >= 100)
 		_putchar((n % 1000) / 100 + '0');
-	if (n >=10)
+	if (n >= 10)
 		_putchar((n % 100) / 10 + '0');
 	_putchar(n % 10 + '0');
 }
