24f6dccdc9c2752ededaf2f2817508b1ee7c3419
Thinker <alphacentuarian@gmail.com>
2022-09-19 13:59:17 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* rev_string - Entry point
+*@s: pointer
+* Return: Always 0 (Success)
+*/
+void rev_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+i	for (; s[i - 1] != '\0'; i--)
+	{
+		_putchar(s[i - 1]);
+	}
+}
+
