be373b8c5b010993880683e6078f518e47e49057
Thinker <alphacentuarian@gmail.com>
2022-09-19 13:48:52 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_rev - Entry point
+*@s: pointer
+* Return: Always 0 (Success)
+*/
+void print_rev(char *s);
+{
+	int i;
+	int rev
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	for (; s[i] != '\0'; i--)
+	{
+		_putchar(s[i]);
+	}
+}
+
