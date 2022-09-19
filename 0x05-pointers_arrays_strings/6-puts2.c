2a6828dc42507a5858a1addc95e8bd79f21732dd
Thinker <alphacentuarian@gmail.com>
2022-09-19 14:46:23 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,13 +8,13 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 	}
-	for (; s[i - 1] != '\0'; i--)
+	for (; str[i - 1] != '\0'; i--)
 	{
 		if (i % 2 != 0)
-			_putchar(s[i - 1]);
+			_putchar(str[i - 1]);
 	}
 	_putchar('\n');
 }
