5b1482c875682a288f49abef30ea1e3f652ee671
Thinker <alphacentuarian@gmail.com>
2022-09-19 18:22:20 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_array - Entry point
+*@a: pointer
+*@n: integer
+* Return: Always 0 (Success)
+*/
+void print_array(int *a, int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		if (i == n-1)
+			printf("%d", a[i]);
+		printf("%d ", a[i]);
+	}
+}
+
