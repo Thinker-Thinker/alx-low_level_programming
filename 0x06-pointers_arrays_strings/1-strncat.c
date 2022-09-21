5efff7bcacf859c1f98797df9fa18a3d90d99f9c
Thinker <alphacentuarian@gmail.com>
2022-09-21 13:45:31 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* _strncat - Entry point
+*@dest: pointer
+*@src: pointer
+* Return: Always 0 (Success)
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+
+	for (j = 0; j <= n; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	return (dest);
+}
+
+
