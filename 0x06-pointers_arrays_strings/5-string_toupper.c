381a8ea66cf4963084cfad0f8b4ce1b895dd5fef
Thinker <alphacentuarian@gmail.com>
2022-09-22 10:08:27 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* string_toupper - Entry point
+*s: pointer
+*
+* Return: Always 0 (Success)
+*/
+char *string_toupper(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+			s[i] = s[i] - 32;
+		i++;
+	}
+	return (s);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,6 +4,8 @@
 
 char *_strncpy(char *dest, char *src, int n);
 
+char *string_toupper(char *);
+
 void reverse_array(int *a, int n);
 
 char *_strcat(char *dest, char *src);
