8f2b109e91a007e761b7c6a5d263bf754ffb17a7
Thinker <alphacentuarian@gmail.com>
2022-09-22 14:48:58 -0700

    102-magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,7 +16,6 @@ int main(void)
 * - only one statement
 * - you are not allowed to code anything else than this line of code
 */
-	printf("n is :%p\n a is :%p\n p is :%p\n a[0] is :%p\n a[1] is :%p\n a[2] is :%p\n a[3] is :%p\n a[4] is %p\n a[5] is %p\n",&n, &a, &p, &a[0],&a[1], &a[2], &a[3], &a[4], &a[5]);
 	*(p + 5) = 98;
 /* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
