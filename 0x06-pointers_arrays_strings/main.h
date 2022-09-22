d885c7f83ed2a2512b3f2fc1004bbfa98a80ab2b
Thinker <alphacentuarian@gmail.com>
2022-09-21 17:02:50 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,6 +6,8 @@ char *_strncpy(char *dest, char *src, int n);
 
 char *_strcat(char *dest, char *src);
 
+int _strcmp(char *s1, char *s2);
+
 char *_strncat(char *dest, char *src, int n);
 
 char *_strcpy(char *dest, char *src);
