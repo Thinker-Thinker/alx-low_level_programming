9e9a03e2af34192d21ed600d66a543f4e1d1441d
Thinker <alphacentuarian@gmail.com>
2022-09-26 15:28:13 -0700

    main.h

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _memset - Entry point
+*@s: pointer
+*@b: character
+*@n: integer
+* Return: Always 0 (Success)
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
