243e01ed4457fb060fb83eccd9b385bfe29482ce
Thinker <alphacentuarian@gmail.com>
2022-09-26 18:40:43 -0700

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* print_diagsums - Entry point
+*@a: pointer
+*@size: integer
+* Return: Always 0 (Success)
+*/
+void print_diagsums(int *a, int size)
+{
+	unsigned int i, j, k, l, m, n;
+
+	k = 0;
+	l = 0;
+	m = 0;
+	n = 0;
+	for (j = 0; j < size; j++)
+	{
+		k = (size * j) + j;
+		m += a[k];
+
+	}
+	for (i = 0; i < size; i++)
+	{
+		l = (size * i) + (size - 1 - i);
+		n += a[l];
+	}
+	printf ("%li %li\n", k, n);
+		
+	
+}
