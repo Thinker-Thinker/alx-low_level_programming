ec3d85686e526862ef18f1c6a0ba2e1d829967a1
Thinker <alphacentuarian@gmail.com>
2022-09-26 17:40:32 -0700

    5-strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,20 +10,25 @@ char *_strstr(char *haystack, char *needle)
 {
 	unsigned int i, j;
 
-	for (i = 0; haystack[i]; i++)
+	i = 0;
+	j = 0;
+	while(haystack[i])
 	{
-			j = 0; 
-			while (haystack[i + j] && needle[j] && (haystack[i] == needle[0]))
-			{
-				if (haystack[i + j] == needle[j])
-					j++;
-				else
-					break;
-			}
-			if (needle[j])
-				break;
+		j = 0; 
+		while (needle[j] && (haystack[i] == needle[0]))
+		{
+			if (haystack[i + j] == needle[j])
+				j++;
 			else
-				return (haystack + i);
+				break;
+		}
+		if (needle[j])
+		{
+			i++;
+			j = 0;
+		}
+		else
+			return (haystack + i);
 
 	}
 	return (0);
