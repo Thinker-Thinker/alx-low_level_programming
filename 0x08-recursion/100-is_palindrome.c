50ebd6b6aad3767c18605c990df661d593531dca
Thinker <alphacentuarian@gmail.com>
2022-09-28 18:07:04 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -26,7 +26,7 @@ int is_palindrome(char *s)
 	i = 0;
 	if (*s == ' ')
 		return (1);
-	j = _print_rev_recursion(s)
+	j = _print_rev_recursion(s);
 	if (s[i] ==j[i])
 	{
 		for (i = 0; s[i] == j[i]; i++)
