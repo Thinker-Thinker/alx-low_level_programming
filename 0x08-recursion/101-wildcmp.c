99b0d159614bf881b8d21f2348b69dbcef70334b
Thinker <alphacentuarian@gmail.com>
2022-09-28 20:05:01 -0700

    101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* _print_rev_recursion - Entry point
+*@s: pointer
+* Return: Always 0 (Success)
+*/
+int wildcmp(char *s1, char *s2)
+{
+	int i;
+	if (*s1 == *s2 && *s1 == '\0')
+		return (1)
+	if (*s1 != *s2 && *s2 != '*')
+		return (0);
+	if (*s2 - 1 == '*' && s2 != '*')
+	{
+		if (s2 != s1)
+		{
+			for (i = 0; s1; i++)
+			{
+				*s1 = s1 + 1;
+				if (s1 == s2)
+					continue;
+				return (0)
+			}
+		}
+	}
+	return (wildcmp(s1 + 1, s2 + 1);
+
+
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,6 +2,8 @@
 
 #define HEADER_H_
 
+int wildcmp(char *s1, char *s2);
+
 int is_palindrome(char *s);
 
 int is_prime_number(int n);
