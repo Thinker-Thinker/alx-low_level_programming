5f6c9735443afd2caaf95fdf342d4b83d8c798a7
Thinker <alphacentuarian@gmail.com>
2022-09-28 17:36:39 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,8 +10,8 @@ int sqrt_calc(int n, int j)
 	if (j * j == n)
 		return (j);
 	sqrt_calc(n, j + 1);
-		
 }
+
 /**
 *_sqrt_recursion - Entry point
 *@n: pointer
@@ -23,6 +23,6 @@ int _sqrt_recursion(int n)
 		return (-1);
 	if (n == 1)
 		return (1);
-	return (sqrt_calc(n, 1);
+	return (sqrt_calc(n, 1));
 
 }
