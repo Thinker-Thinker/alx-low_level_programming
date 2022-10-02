df6e66e16e72f3164a9bdc2bbd810bcf4bb166e8
Thinker <alphacentuarian@gmail.com>
2022-10-02 08:04:04 -0700

    1-args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
 *
 * Return: always 0
 */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[] __attribute__((unused)))
 {
 	printf("%i\n", argc - 1);
 	return (0);
