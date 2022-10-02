3bb0e0e5fd33f147112dd0fe66dc70943198c2d2
Thinker <alphacentuarian@gmail.com>
2022-10-02 10:57:05 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,12 +1,51 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
+*calc_cent - calculate number of change
+*@sum: number of arguments passed to the function
+*@cent: argument vector of pointers to strings
+*
+* Return: always 0
+*/
+int  calc_cent(int sum, int cent)
+{
+	while (cent >= 25)
+	{
+		cent = cent - 25;
+		sum++;
+	}
+	while (cent >= 10)
+	{
+		cent = cent - 10;
+		sum++;
+	}
+	while (cent >= 5)
+	{
+		cent = cent - 5;
+		sum++;
+	}
+	while (cent >= 2)
+	{
+		cent = cent - 2;
+		sum++;
+	}
+	while (cent >= 1)
+	{
+		cent = cent - 1;
+		sum++;
+	}
+	return (sum);
+
+}
+/**
 * main - prints the name of the program
 *@argc: number of arguments passed to the function
 *@argv: argument vector of pointers to strings
 *
 * Return: always 0
 */
+
+
 int main(int argc, char *argv[])
 {
 	int cent;
@@ -16,7 +55,7 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return (0);
+		return (1);
 	}
 	cent = atoi(argv[1]);
 	if (cent <= 0)
@@ -26,31 +65,7 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		while (cent >= 25)
-		{
-			cent = cent - 25;
-			sum++;
-		}	
-		while (cent >= 10)
-		{
-			cent = cent - 10;
-			sum++;
-		}	
-		while (cent >= 5)
-		{
-			cent = cent - 5;
-			sum++;
-		}
-		while (cent >= 2)
-		{
-			cent = cent - 2;
-			sum++;
-		}
-		while (cent >= 1)
-		{
-			cent = cent - 1;
-			sum++;
-		}
+		sum = calc_cent(sum, cent);
 		printf("%i\n", sum);
 		return (0);
 	}
