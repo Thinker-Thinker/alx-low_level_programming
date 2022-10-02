c1cbd6b668863a4dc27a2214cf6425abcbc5a77a
Thinker <alphacentuarian@gmail.com>
2022-10-02 08:08:52 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - prints the name of the program
+*@argc: number of arguments passed to the function
+*@argv: argument vector of pointers to strings
+*
+* Return: always 0
+*/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++) 
+		printf("%s\n", argv[i]);
+	return (0);
+
+}
