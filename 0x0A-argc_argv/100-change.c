a285d04ef0c4baa48b83736416da482b0271bbab
Thinker <alphacentuarian@gmail.com>
2022-10-02 10:34:12 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -7,49 +7,51 @@
 *
 * Return: always 0
 */
-int main(int argc __attribute__((unused))
-, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[])
 {
 	int cent;
 	int sum;
 
 	sum = 0;
-	cent = atoi(argv[1]);
 	if (argc != 2)
 	{
-		puts("Error");
-		return (1);
+		printf("Error\n");
+		return (0);
 	}
+	cent = atoi(argv[1]);
 	if (cent <= 0)
 	{
 		puts("0");
 		return (1);
 	}
-	while (cent >= 25)
+	else
 	{
-		cent = cent - 25;
-		sum++;
-	}	
-	while (cent >= 10)
-	{
-		cent = cent - 10;
-		sum++;
-	}	
-	while (cent >= 5)
-	{
-		cent = cent - 5;
-		sum++;
+		while (cent >= 25)
+		{
+			cent = cent - 25;
+			sum++;
+		}	
+		while (cent >= 10)
+		{
+			cent = cent - 10;
+			sum++;
+		}	
+		while (cent >= 5)
+		{
+			cent = cent - 5;
+			sum++;
+		}
+		while (cent >= 2)
+		{
+			cent = cent - 2;
+			sum++;
+		}
+		while (cent >= 1)
+		{
+			cent = cent - 1;
+			sum++;
+		}
+		printf("%i\n", sum);
+		return (0);
 	}
-	while (cent >= 2)
-	{
-		cent = cent - 2;
-		sum++;
-	}
-	while (cent >= 1)
-	{
-		cent = cent - 1;
-		sum++;
-	}
-	printf("%i\n", sum);
-	return (0);
 }
