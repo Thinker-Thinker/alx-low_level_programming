81ebe168060c245be1c46ab1e96397c31864272d
Thinker <alphacentuarian@gmail.com>
2022-10-02 08:20:03 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* main - prints the name of the program
+*@argc: number of arguments passed to the function
+*@argv: argument vector of pointers to strings
+*
+* Return: always 0
+*/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	int i;
+	int sum;
+
+	sum = 1;
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for(i= 1; i <= 2; i++)
+		sum = sum * argv[i];
+	printf("%i\n", sum);
+	return (0);
+
+}
