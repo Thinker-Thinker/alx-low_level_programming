447ebd9fbbe9920f32844820e535c232670a64e0
Thinker <alphacentuarian@gmail.com>
2022-10-02 10:08:56 -0700

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+/**
+* main - prints the name of the program
+*@argc: number of arguments passed to the function
+*@argv: argument vector of pointers to strings
+*
+* Return: always 0
+*/
+int main(int argc, char *argv[])
+{
+	int cent;
+	int sum;
+
+	sum = 0;
+	cent = atoi(argv[1]);
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	while (cent >= 25)
+	{
+		cent = cent - 25;
+		sum++;
+	}	
+	while (cent >= 10)
+	{
+		cent = cent - 10;
+		sum++;
+	}	
+	while (cent >= 5)
+	{
+		cent = cent - 5;
+		sum++;
+	}
+	while (cent >= 2)
+	{
+		cent = cent - 2;
+		sum++;
+	}
+		while (cent >= 1)
+	{
+		cent = cent - 1;
+		sum++;
+	}
+	printf("%i\n", sum);
+	return (0);
+}
