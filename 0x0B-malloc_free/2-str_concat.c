d8efeee0c3b3be8793492b5b8b0d1cb27f0788d4
Thinker <alphacentuarian@gmail.com>
2022-10-05 03:05:09 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -34,7 +34,12 @@ char *str_concat(char *s1, char *s2)
 	for (j = 0; j < i; j++)
 		a[j] = s1[j];
 	for (j = 0; j < k; j++)
-		a[j + i - 1] = s2[j];
+	{
+		if ( i != 0)
+			a[j + i - 1] = s2[j];
+		else
+			a[j] = s2[j];
+	}
 	return (a);
 }
 
