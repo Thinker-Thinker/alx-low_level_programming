44695c7c570596106ee60b4b255b7586ac78bcc1
Thinker <alphacentuarian@gmail.com>
2022-10-05 21:32:30 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,12 +12,11 @@ char *argstostr(int ac, char **av)
 	char *a;
 	int i, j, l, k;
 
-	i = 0;
 	k = 0;
 	l= 0;
 	if (ac <= 0 || av == NULL)
 		return (NULL);
-	for (i = 0; i > ac; i++)
+	for (i = 0; i < ac; i++)
 	{		
 		for (k = 0; av[i][k]; k++)
 			l++;
