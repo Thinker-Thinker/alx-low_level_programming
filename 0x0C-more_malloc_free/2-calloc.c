1142c5ae64d797edd8673918aa881557df051279
Thinker <alphacentuarian@gmail.com>
2022-10-07 16:52:48 -0700

    2-calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,15 +8,15 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *a;
-	int i;
+	unsigned int i;
 	
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	a = malloc(nmemb * size);
-	if (a == NULL)
-		return (NULL);
 	for (i = 0; a[i]; i++)
 		a[i] = '0';
+	if (a == NULL)
+		return (NULL);
 	return (a);
 }
 
