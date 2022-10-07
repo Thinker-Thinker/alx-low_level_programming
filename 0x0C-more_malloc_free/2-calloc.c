e1cd88c5d3ee366c4687b15166f63b9c381a8e08
Thinker <alphacentuarian@gmail.com>
2022-10-07 16:29:56 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+/**
+* malloc_checked - Entry point
+*@b: character value of integer c
+* Return: Always 0 (Success)
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *a;
+	int i;
+	
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	a = malloc(nmemb * size);
+	if (a == NULL)
+		return (NULL);
+	for (i = 0; a[i]; i++)
+		a[i] = '0';
+	return (a);
+}
+
