5ba987eb4f8df7febc47ddfe32488dbefc7d28d4
Thinker <alphacentuarian@gmail.com>
2022-10-10 17:10:42 -0700

    2-

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,12 +14,12 @@ void print_dog(struct dog *d)
 		if (d->name != NULL)
 			printf("Name: %s\n", d->name);
 		else
-			printf("Name: (nil)\n");
+			printf("Name: (nil) \n");
 		printf("Age: %7f\n", d->age);
 		if (d->owner != NULL)
 			printf("owner: %s\n", d->owner);
 		else
-		printf("owner: (nil)\n");
+		printf("owner: (nil) \n");
 	}
 
 }
