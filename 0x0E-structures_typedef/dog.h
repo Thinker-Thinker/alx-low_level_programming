6dcb5400fdc8675e55b9e0e20c7013c05b7d9165
Thinker <alphacentuarian@gmail.com>
2022-10-10 17:54:37 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,6 +19,14 @@ struct dog
 
 };
 
+/**
+* dog_t - Typedef for struct dog
+*/
+
+typedef struct dog dog_t;
+
+int _putchar(char);
+
 void print_dog(struct dog *d);
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
