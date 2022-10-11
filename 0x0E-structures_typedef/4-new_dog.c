05131f7b03b241bf5870ac283c904799c44b0fbb
Thinker-Thinker <111667135+Thinker-Thinker@users.noreply.github.com>
2022-10-11 02:12:36 +0100

    Omo!

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,21 +1,47 @@
+#include <stdlib.h>
 #include "dog.h"
-#include <stdio.h>
 
 /**
- * print_dog - initializes a structure of type dog
- * @d: pointer to structure
+ * new_dog - creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
  *
- * Return: void
+ * Return: pointer to new dog
  */
-void print_dog(struct dog *d)
+dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (d == NULL)
-		return;
-	else
-	{
-		printf("Name: %s\n", d->name == NULL? "(nil)": d->name);
-		printf("Age: %7f\n",  d->age);
-		printf("owner: %s\n", d->owner == NULL? "(nil)": d->owner);
-	}
+	unsigned int nl, ol, i;
+	dog_t *dog;
 
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	for (nl = 0; name[nl]; nl++)
+		;
+	nl++;
+	dog->name = malloc(nl * sizeof(char));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < nl; i++)
+		dog->name[i] = name[i];
+	dog->age = age;
+	for (ol = 0; owner[ol]; ol++)
+		;
+	ol++;
+	dog->owner = malloc(ol * sizeof(char));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < ol; i++)
+		dog->owner[i] = owner[i];
+	return (dog);
 }
