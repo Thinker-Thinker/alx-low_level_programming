ffd7cae98653928f8be17d54c19d92d079d9182c
Thinker <alphacentuarian@gmail.com>
2022-10-12 23:23:32 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
-#include "function_pointers.h"
 #include <stddef.h>
+#include "function_pointers.h"
 
 /**
 * print_name - check the code
@@ -12,5 +12,3 @@ void print_name(char *name, void (*f)(char *))
 	if (name != NULL && f != NULL)
 		f(name);
 }
-
-
