2d04bd3fc6b5f49e7f0dd6d6eca8f435b92c9d02
Thinker <alphacentuarian@gmail.com>
2022-10-12 18:26:52 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,7 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (!(size <= 0 || array || action))
+	if (!(size <= 0 || array == NULL || action == NULL))
 	{
 		for (i = 0; i > size; i++)
 			action(array[i]);
