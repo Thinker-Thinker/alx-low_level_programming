20ce907d3598d1195647fba25ad648acf218194a
Thinker <alphacentuarian@gmail.com>
2022-10-15 19:45:13 -0700

    2-print_strings.c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+* print_strings - it does what it says on the bottle. (-_-)
+*@separator: separates number
+*@n: no of variadic variable
+* Return: Always 0.
+*/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int  i;
+	va_list list;
+
+	va_start(list, n);
+	if (n <= 0)
+	{
+		printf("\n");
+		return;
+	}
+
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%s", va_arg(list, int) == NULL ? "nil": va_arg(list, int));
+		if (separator != NULL)
+			printf("%s", separator);
+	}
+	printf("%s", va_arg(list, int) == NULL ? "nil": va_arg(list, int));
+	va_end(list);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,15 +2,11 @@
 
 #define HEADER_H_
 
+void print_strings(const char *separator, const unsigned int n, ...);
 
 void print_numbers(const char *separator, const unsigned int n, ...);
 
 int sum_them_all(const unsigned int n, ...);
-
-void print_name(char *name, void (*f)(char *));
-
-char *_memset(char *s, char b, unsigned int n);
-
 
 
 #endif
