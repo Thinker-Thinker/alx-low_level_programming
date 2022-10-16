d2b34f17474486a707a18da4ae38838eef0d2268
Thinker <alphacentuarian@gmail.com>
2022-10-15 19:01:59 -0700

    FINALLY

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,6 +14,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list list;
 
 	va_start(list, n);
+	if (n <= 0)
+	{
+		printf("\n");
+		return;
+	}
+
 	for (i = 0; i < n - 1; i++)
 	{
 		printf("%i", va_arg(list, int));
